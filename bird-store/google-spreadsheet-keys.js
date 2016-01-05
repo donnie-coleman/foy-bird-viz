b915e1fee8b3f0680e6d2ce7566a83c343729b8d
Donnie Coleman <donnie.coleman@gmail.com>
2016-01-04 19:29:10 -0500

    added 2016

--- bird-store/google-spreadsheet-keys.js
+++ bird-store/google-spreadsheet-keys.js
@@ -6,6 +6,8 @@
 		return [{year:'2012', key:'0ApY2PdXZDxl2dFphWVRpazlmdmhWcXNNazRndmJ3VWc'},
 		        {year:'2013', key:'0ApY2PdXZDxl2dDAtdU5FX05saTJaYkNKb3ZzdDU5LXc'},
 		        {year:'2014', key:'0ApY2PdXZDxl2dG5WLU80RkI1aHpoQmpjX2R5NUVlWFE'},
-		        {year:'2015', key:'0ApY2PdXZDxl2dDFldExKTDdGejJfS1U5dHdWcFZVOXc'}];
+		        {year:'2015', key:'0ApY2PdXZDxl2dDFldExKTDdGejJfS1U5dHdWcFZVOXc'},
+		        {year:'2016', key:'1APZj3S2WgEXSta_J2mBbOmNIkW2oADYY7btGqW9A5JM'}
+		];
 	}]);
 })();
